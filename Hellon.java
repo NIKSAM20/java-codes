26fb3538b83d1a54147f813a5c7b9caa89b51cb6
Nikhil <nikhilbisht0026@gmail.com>
2022-08-22 20:41:29 +0530

    files renewed

--- Hellon.java
+++
@@ -1,5 +0,0 @@
-public class Hellon{
-	public static void main(String args[]){
-		System.out.println("hello");
-	}
-}
--- cmdl.java
+++
@@ -1,5 +0,0 @@
-public class cmdl{
-	public static void main(String args[]){
-		System.out.print(args[0]);
-	}
-}
