e0dc59d5e6a6112a2004bf9eac67286bc4639022
Nikhil <nikhilbisht0026@gmail.com>
2022-08-22 21:07:00 +0530

    new code added

---
+++ selection_sort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class selection_sort {
+    static void array(int arr[], int n ){
+        
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the limit: ");
+        int n= sc.nextInt();
+
+        int arr[]= new int[n];
+        System.out.print("Enter the values of the array: ");
+        for (int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+        }
+        array(arr,n);
+    }
+}
