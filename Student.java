48929f068ad0f2292c6fcf77a4c4fa04e2862fc1
Nikhil <nikhilbisht0026@gmail.com>
2022-09-06 21:33:40 +0530

    minor changes made

--- Student.java
+++ Student.java
@@ -1,91 +1,26 @@
 class Students {
-
-    String name;
- 
-    int age;
- 
-  
+    String name; 
+    int age;  
  
     public void getInfo() {
  
         System.out.println("The name of this Student is " + this.name);
- 
         System.out.println("The age of this Student is " + this.age);
- 
-    }
- 
- }
- 
- 
- public class Student {
- 
-    public static void main(String args[]) {
- 
-        Student s1 = new Student();
- 
-        s1.name = "Aman";
- 
-        s1.age = 24;
- 
-        s1.getInfo();
- 
- 
-        Student s2 = new Student();
- 
-        s2.name = "Shradha";
- 
-        s2.age = 22;
- 
-        s2.getInfo();
- 
-    }
- 
- }
- 
- Example 2:
- 
- class Pen {
- 
-    String color;
- 
+     }
+}
   
+public class Student {
+     public static void main(String args[]) {
  
-    public void printColor() {
+       Students s1 = new Students();
+       s1.name = "Aman";
+       s1.age = 24;
+       s1.getInfo();
  
-        System.out.println("The color of this Pen is " + this.color);
  
+       Students s2 = new Students();
+       s2.name = "Shradha"; 
+       s2.age = 22; 
+       s2.getInfo();
     }
- 
- }
- 
- 
- public class OOPS {
- 
-    public static void main(String args[]) {
- 
-        Pen p1 = new Pen();
- 
-        p1.color = blue;
- 
- 
-        Pen p2 = new Pen();
- 
-        p2.color = black;
- 
- 
-        Pen p3 = new Pen();
- 
-        p3.color = red;
- 
- 
-        p1.printColor();
- 
-        p2.printColor();
- 
-        p3.printColor();
- 
-    }
- 
- }
- 
- 
+}
